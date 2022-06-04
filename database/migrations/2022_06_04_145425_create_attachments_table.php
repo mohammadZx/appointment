e81dec6d33947cccb62d19bbfffbdd88eda9e391
یادگیری بهتر برنامه نویسی اموزش رایگان برنامه نویسی <mohammad13yazdani@gmail.com>
2022-06-04 20:34:13 +0430

    assets handeling

--- database/migrations/2022_06_04_145425_create_attachments_table.php
+++ database/migrations/2022_06_04_145425_create_attachments_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
             $table->text('src');
-            $table->timestamps();
         });
     }
 
