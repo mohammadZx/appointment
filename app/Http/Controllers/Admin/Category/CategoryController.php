8bdc5559817a83a27f72469e892796af66ef089b
یادگیری بهتر برنامه نویسی اموزش رایگان برنامه نویسی <mohammad13yazdani@gmail.com>
2022-07-18 14:55:38 +0430

    manage category

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -3,9 +3,96 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Options\Uploader;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        $categories = Category::orderBy('id', 'DESC')->get();
+        return view('admin.category.index', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function create(){
+
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required',
+            'image' => 'nullable|file|max:1024|mimes:jpg,bmp,png,webp,jpeg,gif',
+        ]);
+
+        $image = null;
+        if($request->image){
+            $upload = Uploader::add($request->image);
+            $image = $upload->id;
+        }
+
+        $category = new Category();
+        $category->title = $request->title;
+        $category->icon = $request->icon;
+        $category->content = $request->content;
+        $category->thumbnail_id = $image;
+
+        $category->save();
+
+        return redirect()->back()->with('message', [
+            'type' => 'success',
+            'message' => __('app.Item successfully added')
+        ]);
+
+    }
+
+    public function edit(Category $category){
+        return view('admin.category.add-or-update', [
+            'category' => $category
+        ]);
+    }
+
+    public function update(Request $request, Category $category){
+        $request->validate([
+            'title' => 'required',
+            'image' => 'nullable|file|max:1024|mimes:jpg,bmp,png,webp,jpeg,gif',
+        ]);
+
+        if($request->image){
+            Uploader::delete($category->thumbnail_id);
+            $upload = Uploader::add($request->image);
+            $category->thumbnail_id = $upload->id;
+        }
+
+        $category->title = $request->title;
+        $category->icon = $request->icon;
+        $category->content = $request->content;
+        $category->save();
+
+        return redirect()->back()->with('message', [
+            'type' => 'success',
+            'message' => __('app.Item successfully updated')
+        ]);
+    }
+
+    public function destroy(Request $request, Category $category){
+    
+        $request->validate([
+            'cat' => 'required|exists:categories,id|not_in:' . $category->id
+        ]);
+        
+        $category->services()->update([
+            'category_id' => $request->cat
+        ]);
+        
+        $category->delete();
+
+        return redirect()->back()->with('message', [
+            'type' => 'success',
+            'message' => __('app.Item successfully deleted')
+        ]);
+    }
 }
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Dashboard -->
+  <div id="dashboard"> 
+    @include('partials.user.dashboard')   
+	<div class="utf_dashboard_content">
+	<div class="col-lg-12 col-md-12">
+        <div class="add-province">
+            <div class="row">
+            <form class="changestatus-appointment margin-top-10" action="{{route('admin.category.store')}}" enctype="multipart/form-data" method="post">
+                @csrf  
+                 <div class="col-md-6">
+                    <input class="title @error('title') invalid @enderror" required type="text" name="title" value="{{ old('title') }}" placeholder="{{__('app.Title')}}">
+                    @error('title') 
+                        <span class="invalid-messsage">{{$message}}</span>
+                    @enderror
+
+                    <input class="icon @error('icon') invalid @enderror" type="text" name="icon" value="{{ old('icon') }}" placeholder="{{__('app.Icon')}} font awesome and icon minds">
+                    @error('icon') 
+                        <span class="invalid-messsage">{{$message}}</span>
+                    @enderror
+
+                    <input type="file" class="image  @error('image') invalid @enderror" name="image" accept="image/*">
+                    @error('image') 
+                        <span class="invalid-messsage">{{$message}}</span>
+                    @enderror
+                </div>
+                 <div class="col-md-6">
+                    <textarea name="content" class="content  @error('content') invalid @enderror" rows="6.5" placeholder="{{__('app.Content')}}">{{ old('content') }}</textarea>
+                    @error('content') 
+                        <span class="invalid-messsage">{{$message}}</span>
+                    @enderror
+                </div>
+
+                 <div class="col-md-6"><button class="button margin-right-10">{{__('app.Add')}}</button></div>
+              </form>
+            </div>
+        </div>
+        <div class="utf_dashboard_list_box invoices province-list with-icons margin-top-20">
+            <h4>{{__('app.Category management')}}</h4>
+            <ul>
+              @foreach($categories as $category)
+              <li><strong>{{$category->title}} -- {{$category->listings->count()}}</strong></a>
+
+              <form class="changestatus-appointment" action="{{route('admin.category.destroy', $category->id)}}" method="post">
+                @csrf  
+                @method('delete')
+                {{__('app.Transform Services to')}}    
+                    <select name="cat" class="padding-0 width-200px" required>
+                        <option value="">{{__('app.Select Category')}}</option>
+                        @foreach($categories as $tcat)
+                            <option value="{{$tcat->id}}">{{$tcat->title}}</option>
+                        @endforeach
+                    </select>
+                    {{__('app.And delete Category city')}}
+                  <button class="button margin-right-10">{{__('app.Delete')}}</button>
+                  <a class="button margin-right-20" href="{{route('admin.category.edit', $category->id)}}">{{__('app.Edit')}}</a>
+              </form> 
+              @endforeach
+            </ul>
+          </div>
+
+
+        </div>
+        <div class="col-md-12">
+          <div class="footer_copyright_part">{{__('app.Copyright')}}</div>
+        </div>
+      </div>
+	</div>
+    </div>
+  
+
+@endsection
+
+
+@section('scripts')
+<script>
+(function($) {
+try {
+	var jscr1 = $('.js-scrollbar');
+	if (jscr1[0]) {
+		const ps1 = new PerfectScrollbar('.js-scrollbar');
+
+	}
+    } catch (error) {
+        console.log(error);
+    }
+})(jQuery);
+</script>
+
+@endsection
